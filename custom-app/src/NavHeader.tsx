4de9bd23a4a1781a7bde6bcd282fe52f0dfbb149
Luke Oliff <luke@lukeoliff.com>
2025-02-28 13:36:00 +0000

    chore: remove /fern/dist and update readme instructions (#77)
    * chore: ignore fern dist folder
    
    * chore: improve readme instructions now fern/dist is ignored
    
    * chore(workflows): update workflows to build custom components before checks and build
    
    * fix: fixing instrinsic errors in workflows
    
    * chore(workflows): correct workflows environment for runtime
    
    * chore(workflows): correct remaining workflows

--- custom-app/src/NavHeader.tsx
+++ custom-app/src/NavHeader.tsx
@@ -3,27 +3,56 @@ export function NavHeader() {
     <div className="tw-absolute tw-top-0 tw-inset-x-0 tw-backdrop-blur-2xl h-12 tw-z-30 tw-flex tw-items-center tw-bg-[#232329]">
       <div className="tw-relative tw-w-full tw-flex tw-justify-end tw-items-center tw-max-w-[var(--spacing-page-width)] tw-mx-auto tw-px-4 md:tw-px-6 lg:tw-px-8">
         <div className="tw-flex tw-gap-3 tw-items-center">
-          <a href="https://playground.deepgram.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://playground.deepgram.com"
+            target="_blank"
+            rel="noopener noreferrer">
             <button className="tw-rounded-md tw-text-[#BBBBBF] tw-py-2 tw-px-3 tw-flex tw-items-center tw-gap-2">
-              <img src="https://fern-image-hosting.s3.us-east-1.amazonaws.com/deepgram/sparkle.svg" width="16" height="16" alt="sparkle" />
+              <img
+                src="https://fern-image-hosting.s3.us-east-1.amazonaws.com/deepgram/sparkle.svg"
+                width="16"
+                height="16"
+                alt="sparkle"
+              />
               <span>Playground</span>
             </button>
           </a>
-          <a href="https://console.deepgram.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://console.deepgram.com"
+            target="_blank"
+            rel="noopener noreferrer">
             <button className="tw-rounded-md tw-text-[#BBBBBF] tw-py-2 tw-px-3 tw-flex tw-items-center tw-gap-2">
-              <img src="https://fern-image-hosting.s3.us-east-1.amazonaws.com/deepgram/Key.svg" width="16" height="16" alt="key" />
+              <img
+                src="https://fern-image-hosting.s3.us-east-1.amazonaws.com/deepgram/Key.svg"
+                width="16"
+                height="16"
+                alt="key"
+              />
               <span>Console</span>
             </button>
           </a>
           <a href="/" rel="noopener noreferrer">
             <button className="tw-rounded-md tw-text-[#BBBBBF] tw-py-2 tw-px-3 tw-flex tw-items-center tw-gap-2">
-              <img src="https://fern-image-hosting.s3.us-east-1.amazonaws.com/deepgram/docs-open-book.svg" width="16" height="16" alt="docs" />
+              <img
+                src="https://fern-image-hosting.s3.us-east-1.amazonaws.com/deepgram/docs-open-book.svg"
+                width="16"
+                height="16"
+                alt="docs"
+              />
               <span>Documentation</span>
             </button>
           </a>
-          <a href="https://community.deepgram.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://community.deepgram.com"
+            target="_blank"
+            rel="noopener noreferrer">
             <button className="tw-rounded-md tw-text-[#BBBBBF] tw-py-2 tw-px-3 tw-flex tw-items-center tw-gap-2">
-              <img src="https://fern-image-hosting.s3.us-east-1.amazonaws.com/deepgram/community-users.svg" width="16" height="16" alt="community" />
+              <img
+                src="https://fern-image-hosting.s3.us-east-1.amazonaws.com/deepgram/community-users.svg"
+                width="16"
+                height="16"
+                alt="community"
+              />
               <span>Community</span>
             </button>
           </a>
